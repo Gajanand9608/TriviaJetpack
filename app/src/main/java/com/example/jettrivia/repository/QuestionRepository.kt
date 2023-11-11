dc06bbaa0def343eb4222200bf2911489f550924
Gajanand9608 <gajanandsharmajsr@gmail.com>
2023-11-11 22:08:31 +0530

    question_repository_done

--- app/src/main/java/com/example/jettrivia/repository/QuestionRepository.kt
+++ app/src/main/java/com/example/jettrivia/repository/QuestionRepository.kt
@@ -1,10 +1,28 @@
 package com.example.jettrivia.repository
 
+import android.util.Log
 import com.example.jettrivia.data.DataOrException
 import com.example.jettrivia.model.QuestionItem
 import com.example.jettrivia.network.QuestionAPI
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val questionAPI: QuestionAPI) {
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+    private val questionItemListOrException =
+        DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            questionItemListOrException.loading = true
+            val response = questionAPI.getAllQuestions()
+            questionItemListOrException.data = response
+            if (questionItemListOrException.data.toString()
+                    .isNotEmpty()
+            ) questionItemListOrException.loading = false
+
+        } catch (exception: Exception) {
+            questionItemListOrException.e = exception
+            Log.d("TAG", "getAllQuestions: ${questionItemListOrException.e!!.localizedMessage} ")
+        }
+        return questionItemListOrException
+    }
 }
